f4127f62f9f79a08dea7ae9ade142df08dfec456
sabrina-s <tesmemepasla@gmail.com>
2017-07-05 15:27:55 +0800

    Admin can add, edit, and delete projects

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -1,47 +1,6 @@
 class Api::ProjectsController < ApplicationController
-  before_action :authenticate_admin!
-  before_action :set_project, only: [:edit, :update, :destroy]
-
   def index
     @projects = Project.all
     render json: @projects
   end
-
-  def new
-    @project = Project.new
-  end
-
-  def create
-    @project = Project.new(project_params)
-
-    if @project.save
-      redirect_to api_projects_path
-    else
-      render :new
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    @project.update(project_params)
-    redirect_to api_projects_path
-  end
-
-  def destroy
-    @project.destroy
-    redirect_back(fallback_location: api_projects_path)
-  end
-
-  private
-
-  def set_project
-    @project = Project.find(params[:id])
-  end
-
-  def project_params
-    params.require(:project).permit(:title, :description)
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root to: 'api/projects#index'
   devise_for :admins
   namespace :api do
+    resources :projects, only: [:index]
+  end
+
+  namespace :admin do
     resources :projects
   end
 
